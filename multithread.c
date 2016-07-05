244d58af0e22d4b39e7450901bde075c293a27a1
ricardo-silveira <ricardosilveira@poli.ufrj.br>
2016-07-05 11:10:49 -0300

    basic multithread example

---
+++ multithread.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int g = 0;
+
+void *myThreadFun(void *vargp){
+    int myId = (int)vargp;
+    static int s = 0;
+    ++s;
+    ++g;
+    printf("Thread ID: %d, Static: %d, Global: %d\n", myId, ++s, ++g);
+}
+
+int main(){
+    int i;
+    pthread_t tId;
+    for(i=0; i<3; i++)
+        pthread_create(&tId, NULL, myThreadFun, (void *)i);
+    pthread_exit(NULL);
+    return 0;
+}
